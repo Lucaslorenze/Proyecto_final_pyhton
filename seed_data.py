ff03054756822a9e05a01fc4288adc5c7938f72c
Lucas Lorenze <lucas.lorenze@gmail.com>
2022-10-18 22:03:22 -0300

    Test

---
+++ seed_data.py
@@ -0,0 +1,8 @@
+from AppLucas.models import Familiar
+
+Familiar(nombre="Alberto", direccion="Cochrane 4481", numero_pasaporte=3928293).save()
+Familiar(nombre="Carina", direccion="Olazabal 5923", numero_pasaporte=5243245).save()
+Familiar(nombre="Esteban", direccion="Alsina 1433", numero_pasaporte=38765678).save()
+Familiar(nombre="Carla", direccion="Emilio Lamarca 3948", numero_pasaporte=12365323).save()
+
+print("Se cargo con éxito los usuarios de pruebas")
