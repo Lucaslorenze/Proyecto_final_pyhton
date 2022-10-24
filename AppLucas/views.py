4a655f7b2d772c9f14174f563c940002418911e3
Lucas Lorenze <lucas.lorenze@gmail.com>
2022-10-24 16:41:52 -0300

    Se agrego la funcion "Agregar"

--- AppLucas/views.py
+++ AppLucas/views.py
@@ -2,7 +2,7 @@ from multiprocessing import reduction
 from django.shortcuts import render
 from django.http import HttpResponse
 from AppLucas.models import Familiar
-from AppLucas.forms import Buscar
+from AppLucas.forms import Buscar, FamiliarForm
 from django.views import View
 
 # Create your views here.
@@ -44,3 +44,25 @@ class BuscarFamiliar(View):
             return render(request, self.template_name, {'form':form, 
                                                         'lista_familiares':lista_familiares})
         return render(request, self.template_name, {"form": form})
+
+class AltaFamiliar(View):
+
+    form_class = FamiliarForm
+    template_name = 'AppLucas/alta_familiar.html'
+    initial = {"nombre":"", "direccion":"", "numero_pasaporte":""}
+
+    def get(self, request):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            msg_exito = f"se cargo con éxito el familiar {form.cleaned_data.get('nombre')}"
+            form = self.form_class(initial=self.initial)
+            return render(request, self.template_name, {'form':form, 
+                                                        'msg_exito': msg_exito})
+        
+        return render(request, self.template_name, {"form": form})
+
--- el_proyecto/urls.py
+++ el_proyecto/urls.py
@@ -1,21 +1,6 @@
-"""el_proyecto URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from AppLucas.views import mostrar_mi_template, saludo, saludo_dos, monstrar_familiares, BuscarFamiliar
+from AppLucas.views import mostrar_mi_template, saludo, saludo_dos, monstrar_familiares, BuscarFamiliar, AltaFamiliar
 from blog.views import index as blog_index
 
 urlpatterns = [
@@ -26,4 +11,5 @@ urlpatterns = [
     path('mi-familia/', monstrar_familiares),
     path("blog/", blog_index),
     path("mi-familia/buscar", BuscarFamiliar.as_view()),
+    path('mi-familia/alta', AltaFamiliar.as_view())
 ]
