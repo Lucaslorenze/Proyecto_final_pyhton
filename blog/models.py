6a07f16e5db6d47fa60ecc100293ba917d6dac35
Lucas Lorenze <lucas.lorenze@gmail.com>
2022-10-24 17:58:38 -0300

    Se agrego acceso al admin y cambio en el blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Configuracion(models.Model):
+    nombre_blog = models.CharField(max_length = 14)
+    construido_por = models.CharField(max_length = 30)
+    titulo_principal = models.CharField(max_length = 120)
